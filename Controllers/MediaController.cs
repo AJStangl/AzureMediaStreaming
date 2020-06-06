aacbb26d73cd46832ac4cc2fb3bbcf80a025884d
AJStangl <ajstangl@gmail.com>
2020-06-06 13:35:33 -0400

    No clue

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -10,11 +10,13 @@ using Microsoft.Extensions.Logging;
 
 namespace AzureMediaStreaming.Controllers
 {
+    [ApiController]
     [Route("[controller]")]
     public class MediaController : ControllerBase
     {
         private readonly IAzureMediaService _azureMediaService;
         private readonly ILogger<MediaController> _logger;
+
         public MediaController(IAzureMediaService azureMediaService, ILogger<MediaController> logger)
         {
             _azureMediaService = azureMediaService;
@@ -45,33 +47,17 @@ namespace AzureMediaStreaming.Controllers
                 return BadRequest(e);
                 throw;
             }
-
         }
 
         [HttpGet]
-        [Route("[action]")]
-        public async Task<IActionResult> TestJson()
+        public VideoModel Get()
         {
-            // TODO: Implement everything
-            string locatorName = "locator-c7943896de4d4cb6a6484fc878028fd7";
-            _logger.LogInformation("Getting streaming");
-            try
+            return new VideoModel
             {
-                var videoUrls = await _azureMediaService.GetStreamingUrlsAsync(locatorName);
-                string videoUrl = videoUrls.FirstOrDefault();
-
-                return Ok(new VideoModel
-                {
-                    VideoName = "Demo Video",
-                    VideoUrl = videoUrl
-                });
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "An error has occured trying to obtain data");
-                return BadRequest(e);
-            }
-
+                VideoName = "Demo Video",
+                VideoUrl =
+                    "https://ajstangl-usea.streaming.media.azure.net/fc8b5b35-a2ca-4dbf-99c8-4740726e2529/Ignite-short.ism/manifest(format=m3u8-aapl)"
+            };
         }
 
         [HttpGet]
